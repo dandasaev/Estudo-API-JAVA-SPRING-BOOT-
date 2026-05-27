ef8c0a44080a0946bf240e071cee2f7e8b11489e
Daniel Dasaev <dandasaev@gmail.com>
2026-05-27 17:15:34 -0300

    post cadastro medicos dto

---
+++ api/api/src/main/java/med/voll/api/medico/DadosEndereco.java
@@ -0,0 +1,4 @@
+package med.voll.api.medico;
+
+public record DadosEndereco(String logradouro, String Bairro, String cep, String cidade, String estado, String uf, String complemento, String numero) {
+}
