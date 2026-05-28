b26d2f9a8c81159ef01736aacd80dd826df7cfba
Daniel Dasaev <dandasaev@gmail.com>
2026-05-27 22:15:06 -0300

    inclusao de telefone na tabela

--- api/api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,4 +1,31 @@
-package med.voll.api.medico;
+package med.voll.api.endereco;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String numero;
+
+    public Endereco(DadosEndereco endereco) {
+        this.cep = endereco.cep();
+        this.logradouro = endereco.logradouro();
+        this.complemento = endereco.complemento();
+        this.bairro = endereco.bairro();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+        this.numero = endereco.numero();
+
+
+    }
 }
